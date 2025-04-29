f0325c0957de3a49d6becdf0fa53235d20e4f8a1
Santiago Morales <s.moralesvranic@gmail.com>
2025-04-29 10:30:35 -0300

    Added: push + tests, and changed the power of 2 checker

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod merkle_tree;
 fn main() {
-    let test_tree = merkle_tree::new(&[0; 5]);
-    print!("{:?}", test_tree.get_tree());
+    let mut test_tree = merkle_tree::new(&[0; 4]);
+    let test_tree_2 = merkle_tree::new(&[0; 5]);
+    test_tree.push(&0);
+    print!("========================================");
+    print!("{:?} ", test_tree.get_tree());
+    print!("========================================");
+    print!("{:?} ", test_tree_2.get_tree());
+    print!("========================================");
 }
--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,6 +1,7 @@
 use std::hash::{DefaultHasher, Hash, Hasher};
 pub struct MerkleTree {
     tree: Vec<u64>,
+    leaf_amount: usize,
 }
 impl MerkleTree {
     fn generate_tree<H: Hash>(&mut self, data: &[H]) {
@@ -11,35 +12,45 @@ impl MerkleTree {
             add_hash(d, &mut hashed_tree);
         }
         // Check if the amount of nodes is a power of two, if not complete it with 0
-        if !is_power_of_2(hashed_tree.len() as u128) {
-            let power_of_2 = closest_power_of_2(hashed_tree.len() as u128);
-            loop {
-                if power_of_2 == hashed_tree.len() as u128 {
-                    break;
-                }
-                add_hash(&0, &mut hashed_tree);
-            }
-        }
+        add_padding(&mut hashed_tree);
         // Reverse hashed_tree so that the combined hashes can be pushed
         hashed_tree.reverse();
+        self.tree = hashed_tree;
+        self.generate_from_hashes();
+    }
+
+    fn generate_from_hashes(&mut self) {
         // Calculate the amount of nodes the binary tree will have
         // number_of_nodes = 2 * amount_of_leafs - 1
-        let number_of_nodes = 2 * hashed_tree.len() - 1;
+        let number_of_nodes = 2 * self.tree.len() - 1;
         // Calculate the combined hashes and push it
         let mut index = 0;
         loop {
-            let left_data = hashed_tree[index];
-            let right_data = hashed_tree[index + 1];
-            add_two_hashes(&left_data, &right_data, &mut hashed_tree);
+            let left_data = self.tree[index];
+            let right_data = self.tree[index + 1];
+            add_two_hashes(&left_data, &right_data, &mut self.tree);
             index += 2;
             if index >= number_of_nodes - 1 {
                 break;
             }
         }
         // Reverse the vector so that the root is the head
-        hashed_tree.reverse();
-        // Set the value into the tree
-        self.tree = hashed_tree.clone();
+        self.tree.reverse();
+    }
+
+    pub fn push<H: Hash>(&mut self, data: &H) {
+        let length = self.tree.len();
+        let mut leafs = self.tree[(length - self.leaf_amount)..].to_vec();
+        // Add the new leaf
+        add_hash(data, &mut leafs);
+        // Add padding
+        add_padding(&mut leafs);
+        // Reverse leafs so that the last one is in the head
+        leafs.reverse();
+        self.tree = leafs;
+        self.leaf_amount += 1;
+        // Generate the rest of the tree
+        self.generate_from_hashes();
     }
 
     pub fn get_tree(&self) -> Vec<u64> {
@@ -48,25 +59,12 @@ impl MerkleTree {
 }
 // Creates a new instance of MerkleTree and generates its tree value from the array
 pub fn new<H: Hash>(data: &[H]) -> MerkleTree {
-    let mut initialized_tree = MerkleTree { tree: Vec::new() };
+    let mut initialized_tree = MerkleTree {
+        tree: Vec::new(),
+        leaf_amount: data.len(),
+    };
     initialized_tree.generate_tree(data);
     initialized_tree
-}
-
-fn is_power_of_2(number: u128) -> bool {
-    let mut value = number;
-    loop {
-        // If the value is 2 then return true
-        if value == 2 {
-            return true;
-        }
-        // if the value is odd return false
-        if value % 2 != 0 {
-            return false;
-        }
-        // Neither of the above then divide the value by 2 and continue
-        value /= 2;
-    }
 }
 
 fn closest_power_of_2(number: u128) -> u128 {
@@ -91,6 +89,18 @@ fn add_two_hashes<H: Hash>(left_data: &H, right_data: &H, hashed_tree: &mut Vec<
     (*right_data).hash(&mut hasher);
     hashed_tree.push(hasher.finish());
 }
+// If the leaf amount isn't a power of 2 then add the minimum amount of 0 possible to make it one
+fn add_padding(tree: &mut Vec<u64>) {
+    if !tree.len().is_power_of_two() {
+        let closest_power_of_2 = closest_power_of_2(tree.len() as u128);
+        loop {
+            if closest_power_of_2 == tree.len() as u128 {
+                break;
+            }
+            add_hash(&0, tree);
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -104,5 +114,12 @@ mod test {
             not_power_of_two_tree.get_tree(),
             closes_power_of_two_tree.get_tree()
         );
+    }
+    #[test]
+    fn push_test() {
+        let mut first_tree = new(&[0, 0, 0, 0]);
+        let second_tree = new(&[0, 0, 0, 0, 1]);
+        first_tree.push(&1);
+        assert_eq!(first_tree.get_tree(), second_tree.get_tree());
     }
 }
